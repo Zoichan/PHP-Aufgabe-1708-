aed84131ff3b89a1eb4d97c98ee5081097eb7b54
Zoichan <toru.iris.0@gmail.com>
2017-08-18 00:11:06 +0200

    Update Aufgabe3.php

--- Aufgabe3.php
+++ Aufgabe3.php
@@ -9,7 +9,6 @@ VALUES ('$x1', '$x2', '$x3', '$x4')";
     mysqli_query($sql);    
 }
 
-
 function read() {
     $x1 = $_GET[Ereigniszeitpunkt];
     $sql = "SELECT * FROM Termin WHERE EREIGNISZEITPUNKT= '$x1' ");
@@ -24,14 +23,12 @@ function update() {
     $sql = "SELECT id FROM Termin WHERE Ereigniszeitpunkt = '$x1' ";
     $id = mysqli_query($sql);
     $sql = "UPDATE Termin SET Textnachricht='$x2', Ereigniszeitpunkt ='$x1', Aktualisierungsdatum = '$x4' WHERE id ='$id'";
-mysqli_query($sql);
-    
+mysqli_query($sql);    
 }
 
 function delete() {
     $x1 = $_GET[Ereigniszeitpunkt];
     $sql = "DELETE FROM Termin WHERE Ereigniszeitpunkt ='$x1' ";
     mysqli_query($sql);
-
 }
 
